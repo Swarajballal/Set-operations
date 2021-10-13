d4b80f8af0e7b0f6ad929299bc0ef04664815223
Swarajballal <86348862+Swarajballal@users.noreply.github.com>
2021-10-14 00:00:42 +0530

    setopera.py
    created for task4

---
+++ set.py
@@ -0,0 +1,17 @@
+#Task 4 - Now that we have learnt the basics of Python programming language, let's get our hands dirty and create a new python program. Write a Python Program to Illustrate Different Set Operations.
+#Here we have to define two set variables and we have to perform different set operations: union, intersection, difference and symmetric difference.
+
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+#union
+print("Union of E and N is",E | N)
+
+#intersection
+print("Intersection of E and N is",E & N)
+
+#difference
+print("Difference of E and N is",E - N)
+
+# symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
